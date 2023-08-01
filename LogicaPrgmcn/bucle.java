fcad280c1b79391d5f3c87dcf1bbb01ab9343651
Yber-M <ybermarlon@gmail.com>
2023-08-01 17:00:08 -0500

    Bases de la programación

--- LogicaPrgmcn/bucle.java
+++ LogicaPrgmcn/bucle.java
@@ -1,5 +1,24 @@
 package LogicaPrgmcn;
 
+import java.util.Scanner;
+
 public class bucle {
-    
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        String name;
+        int veces, cont = 1;
+
+        System.out.println("¿Cuál es tu nombre?");
+        name = scan.nextLine();
+
+        System.out.println("¿Cuántas veces quieres que se repita?");
+        veces = scan.nextInt();
+
+        while (cont <= veces) {
+            System.out.print("N° " + cont + " - ");
+            System.out.println(name);
+            cont++;
+        }
+
+    }
 }
